f3793f548a99de32ad3562a9e34f625a85406b36
sohee <totns6543@naver.com>
2020-07-22 15:24:05 +0900

    섹션 6.9까지

---
+++ lecture6_project_soluton/Project3/Project3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	/*int tv, fv;
+	tv = (1 < 2);
+	fv = (1 > 2);
+
+	printf("true is %d\n", tv);
+	printf("false is %d\n", fv);
+	*/
+
+	bool bt = true;
+	bool bf = false;
+
+	printf("true is %d\n", bt);
+	printf("false is %d\n", bf);
+}
